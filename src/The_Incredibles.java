61948254e9f016509bbe68f0c393910e8899f790
oldpincon <ls0968798609@gmail.com>
2022-01-25 19:00:00 +0800

    first commit

---
+++ src/The_Incredibles.java
@@ -0,0 +1,12 @@
+abstract class The_Incredibles {
+     abstract void age();
+     public void clothes(){
+         System.out.println("tights");
+    }
+}
+class dad extends The_Incredibles{
+    public void clothes() {
+        System.out.println("");
+    }
+}
+
